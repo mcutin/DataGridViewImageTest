a2939ab3a3f7ea2d8e23bc7718945281864249ad
Marcelo Cutin <marcelo.cutin@gmail.com>
2022-04-16 18:49:02 -0300

    Add project files.

---
+++ DataGridViewImageTest/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace DataGridViewImageTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvImages = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvImages)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvImages
+            // 
+            this.dgvImages.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvImages.Location = new System.Drawing.Point(12, 12);
+            this.dgvImages.Name = "dgvImages";
+            this.dgvImages.Size = new System.Drawing.Size(240, 150);
+            this.dgvImages.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 177);
+            this.Controls.Add(this.dgvImages);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvImages)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvImages;
+    }
+}
+
---
+++ DataGridViewImageTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewImageTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvImages.Columns.Add(new DataGridViewImageColumn());
+            dgvImages.Rows.Add(3);
+            DataGridViewImageCell cell = (DataGridViewImageCell)dgvImages.Rows[0].Cells[0];
+            cell.Value = Properties.Resources.Low_16x;
+            cell = (DataGridViewImageCell)dgvImages.Rows[1].Cells[0];
+            cell.Value = Properties.Resources.Normal_16x;
+            cell = (DataGridViewImageCell)dgvImages.Rows[2].Cells[0];
+            cell.Value = Properties.Resources.High_16x;
+        }
+    }
+}
